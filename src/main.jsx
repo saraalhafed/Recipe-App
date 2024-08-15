6742a359d6a03df7aae493ce73ceac5ea484bfa6
sara <saraalhafiy89@gmail.com>
2024-08-15 21:30:18 +0200

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,7 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+/* every thing inside the file gitignore will not tracked by github , automaticly all the libraries or package wich take alot of space will be inside it */
+/* i creat a file .env to protact my App-key and App-ID */
+
+/* .env : to protact my data  */
